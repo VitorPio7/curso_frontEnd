78514f3e66fed06b5b96df51b34b7bc24d8c6588
Vitor Pio Vieira <106124241+VitorPio7@users.noreply.github.com>
2024-03-05 23:13:58 -0300

    s
    s

---
+++ Loiane/ex1.js
@@ -0,0 +1,14 @@
+
+var num = 4;
+
+console.log("num: " + num)/*esse concatena tudo em uma única string */
+console.log("num: ", num)/*permite visualizar o conteudo da variavel caso ela seja obj */
+console.log('~5:', (~5))
+
+console.log("-----------------------------------------------------------")
+
+var myObj = { name: 'John', age: 21 };
+delete myObj.name;
+console.log(myObj)
+
+console.log("-----------------------------------------------------------")
